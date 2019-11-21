92d205b1f91480edd8dab9adc4573f9ef38dc46d
ArunavaM <arunava6mondal@gmail.com>
2019-11-21 20:53:52 +0530

    Homepage move to page folder and implement react-router-dom

--- src/App.js
+++ src/App.js
@@ -1,11 +1,26 @@
 import React from 'react';
 import './App.css';
-import HomePage from './components/homepage/homepage.component';
+import { Route,Switch } from 'react-router-dom';
+import HomePage from './pages/homepage/homepage.component';
+
+
+const HatPage=()=>(
+  <div>
+    <h1>
+    Hatpage
+    </h1>
+    </div>
+);
 
 function App() {
   return (
     <div>
-      <HomePage />
+      <Switch>
+        <Route exact path='/' component={HomePage}/>
+        <Route path='/hats' component={HatPage} />
+        
+      </Switch>
+      
     </div>
   );
 }
